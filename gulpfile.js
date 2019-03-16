15c4f35ce4d9ea6cf2d264d2532e575bdd6c9960
Masud Afsar <masud.afsar@gmail.com>
2019-03-17 00:46:51 +0330

    Implement live web server
     - add fancy log package
     - add gulp connect package

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const {series, parallel, src, dest} = require('gulp');
+const log = require('fancy-log');
 const clean = require('gulp-clean');
 const ejs = require('gulp-ejs');
 const sass = require('gulp-sass');
 const srcmap = require('gulp-sourcemaps');
+const connect = require('gulp-connect');
 
 sass.compiler = require('node-sass');
 
@@ -15,7 +17,7 @@ function clean_builds() {
 
 function build_html() {
     return src('./src/page/*.ejs')
-        .pipe(ejs({}, {}, {ext: '.html'}))
+        .pipe(ejs({}, {}, {ext: '.htm'}).on('error', log))
         .pipe(dest('./dist'))
 }
 
@@ -27,32 +29,40 @@ function build_styles() {
         .pipe(dest('./dist/css'));
 }
 
-function build_scripts(cb) {
-    cb();
+function build_scripts() {
+    return src('./src/scripts/**/*').pipe(dest('./dist/scripts/'));
 }
 
 function build_statics() {
     return src('./src/static/**/*', {dot: true}).pipe(dest('./dist/'));
 }
 
-exports.default = series(
-    clean,
-    parallel(
-        build_html,
-        build_styles,
-        build_scripts,
-        build_statics
-    )
-);
-exports.clean = clean_builds;
-exports.build = parallel(
+function run_server() {
+    return connect.server({
+        port: 8081,
+        root: './dist/',
+        livereload: true,
+    })
+}
+
+const build = parallel(
     build_html,
     build_styles,
     build_scripts,
     build_statics
 );
+
+const server = series(
+    clean,
+    build,
+    run_server
+);
+
+exports.default = server;
+exports.clean = clean_builds;
 exports.html = build_html;
 exports.style = build_styles;
 exports.script = build_scripts;
 exports.static = build_statics;
-
+exports.build = build;
+exports.server = server;
